9b06f4529eabbf2a948a73289408d819b5432e4b
Zorenkoi <zorenkoi956@gmail.com>
2023-08-21 19:59:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+new Swiper('.project-img-container', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  // navigation: {
+  //   prevEl: '.companies-prev',
+  //   nextEl: '.companies-next',
+  // },
+  // breakpoints: {
+  //   100: {
+  //     slidesPerView: 1,
+  //   },
+  //   520: {
+  //     slidesPerView: 2,
+  //   },
+  //   740: {
+  //     slidesPerView: 3,
+  //   },
+  //   850: {
+  //     slidesPerView: 4,
+  //   },
+  // },
+})
